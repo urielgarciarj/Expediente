6b7a50281e2ba4f06f01a2d8c2639625e0594940
Uriel <urielgarciarj@outlook.com>
2019-11-03 16:29:27 -0600

    Form de Busqueda añadido

---
+++ Expediente_ArbolBinario/Busqueda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expediente_ArbolBinario
+{
+    public partial class Busqueda : Form
+    {
+        public Busqueda()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
